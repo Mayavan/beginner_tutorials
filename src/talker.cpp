a930357ef18b049877130bfc2cbc6578a0daf55f
mayavan <mayavan95@gmail.com>
2018-11-05 21:17:16 -0500

    Added Doxygen comments to talker and listener

--- src/talker.cpp
+++ src/talker.cpp
@@ -1,28 +1,26 @@
-/*
- * Copyright (C) 2008, Morgan Quigley and Willow Garage, Inc.
+/**
+ * @file    talker.cpp
+ * @brief Implementation of publisher node to publish to chatter topic with services to change messages
+ * @author RajendraMayavan
+ * @copyright MIT License
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *   * Neither the names of Stanford University or Willow Garage, Inc. nor the
- * names of its contributors may be used to endorse or promote products derived
- * from this software without specific prior written permission.
+ * Copyright (c) 2018 Mayavan
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #include <sstream>
@@ -30,8 +28,13 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
-std::string messageToPublish = "Hello";
+std::string messageToPublish = "Hello"; /*!< Initialization of Message to be published */
 
+/**
+ * @brief Callback function to set the message to be published to chatter topic
+ * @param req input request of service
+ * @param req input response of service
+ */
 bool setMessage(beginner_tutorials::StringService::Request &req,
                 beginner_tutorials::StringService::Response &resp) {
   ROS_INFO_STREAM("The message has been set to "<< req.message);
@@ -44,46 +47,19 @@ bool setMessage(beginner_tutorials::StringService::Request &req,
 }
 
 /**
- * This tutorial demonstrates simple sending of messages over the ROS system.
+ * @brief main function to create the node and publish to chatter topic
+ * @param argc Standard main function parameter
+ * @param argv Standard main function parameter
  */
 int main(int argc, char **argv) {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command
-   * line. For programmatic remappings you can use a different version of init()
-   * which takes remappings directly, but for most command-line programs,
-   * passing argc and argv is the easiest way to do it.  The third argument to
-   * init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
+
+  // ROS initialization
   ros::init(argc, argv, "talker");
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the
-   * last NodeHandle destructed will close down the node.
-   */
+  // Initialization of Node handle
   ros::NodeHandle n;
 
-  /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
-   */
+  // Initialization of publisher
   ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
 
   // Advertise service to change messages
@@ -98,6 +74,7 @@ int main(int argc, char **argv) {
 
   ROS_INFO_STREAM("Loop frequency set to "<< hertz);
 
+  // Show error if frequency is negative or very low or very high
   if (hertz < 1) {
     hertz = 1;
     ROS_WARN_STREAM(
@@ -108,17 +85,12 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
+  // Set the publisher rate
   ros::Rate loop_rate(hertz);
 
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
+  // Counter to count the message number
   int count = 0;
   while (ros::ok()) {
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
     std_msgs::String msg;
 
     std::stringstream ss;
@@ -127,12 +99,7 @@ int main(int argc, char **argv) {
 
     ROS_INFO_STREAM(msg.data.c_str());
 
-    /**
-     * The publish() function is how you send messages. The parameter
-     * is the message object. The type of this object must agree with the type
-     * given as a template parameter to the advertise<>() call, as was done
-     * in the constructor above.
-     */
+    // Publish message to the chatter topic
     chatter_pub.publish(msg);
 
     ros::spinOnce();
